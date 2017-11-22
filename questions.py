e02b342e1f3ece192de4eef20f99faf832cc3759
Caleb Smith-Salzberg <csmithsalzberg@stuy.edu>
2017-11-22 13:12:34 -0500

    questions

---
+++ questions.py
@@ -0,0 +1,31 @@
+import urllib2
+import json
+
+f = open("topcategories.txt")
+catdict = f.read()
+
+def questiondict(cat):
+    retdict = {}
+    if cat not in catdict:
+        print "error"
+        return
+    url = "http://www.jservice.io/api/category?id=" + str(catdict[cat])
+    uResp = urllib2.urlopen( url )
+    raw = uResp.read()
+    dat = json.loads(raw)
+    clues = dat["clues"]
+    
+    x=0
+    i=1
+    while i < 6:
+        if clues[x]["values"] == i*100:
+            retdict[i*100] = [ clues[x]["question"], clues[x]["answer"] ]
+            i += 1
+            x = -1
+        if x >= len(clues):
+            i += 1
+            x = -1
+        x += 1
+    return retdict
+print questiondict('History')
+        
