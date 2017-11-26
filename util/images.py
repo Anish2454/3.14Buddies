15042f891f88b8e67713727f3ebffc6ac5aeea59
csmithsalzberg <caleb.smithsalzberg@gmail.com>
2017-11-26 17:14:16 -0500

    image functionality function added

--- util/images.py
+++ util/images.py
@@ -6,4 +6,11 @@ def getKey():
     key = f.read()
     return key[:-1] #Removes Newline
 
-print getKey()
+def getImage(answer): #input the description of the desired image, which is the answer to the jeopardy question
+    url = urllib2.Request("https://api.gettyimages.com/v3/search/images?sort_order=most_popular&phrase=" + answer, headers={ 'Api-Key' : getKey()})
+    uResp = urllib2.urlopen( url )
+    contentsraw = uResp.read()
+    dat = json.loads(contentsraw)
+    return dat['images'][0]['display_sizes'][0]['uri'] #outputs a url to the image
+
+
