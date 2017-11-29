b893879fe5dd6efbe5ff5212283fc095e6378c1b
csmithsalzberg <caleb.smithsalzberg@gmail.com>
2017-11-28 19:14:04 -0500

    fixed spaces and other weird characters

--- util/images.py
+++ util/images.py
@@ -1,4 +1,5 @@
 import urllib2
+import urllib
 import json
 
 def getKey():
@@ -7,9 +8,12 @@ def getKey():
     return key[:-1] #Removes Newline
 
 def getImage(answer): #input the description of the desired image, which is the answer to the jeopardy question
+    answer = urllib.quote(answer)
     url = urllib2.Request("https://api.gettyimages.com/v3/search/images?sort_order=most_popular&phrase=" + answer, headers={ 'Api-Key' : getKey()})
     print url
     uResp = urllib2.urlopen( url )
     contentsraw = uResp.read()
     dat = json.loads(contentsraw)
     return dat['images'][0]['display_sizes'][0]['uri'] #outputs a url to the image
+
+
